74103ff190d168184a2881ba13640769d0e8de8a
kowsalyasel <skowsy711@gmail.com>
2024-04-09 21:51:22 +0530

    Test

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './routes'
+import './assets/main.css';
+
+
+createApp(App)
+  .use(router) // Use the router instance
+  .mount('#app');
